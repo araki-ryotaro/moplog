7b093d8430e68e4a33063e01cbf329f20f3afb15
araki-ryotaro <monyo.nyomo@gmail.com>
2022-04-10 10:18:45 +0000

    [add]会員側のジャンルコントローラー追記

--- app/controllers/public/genres_controller.rb
+++ app/controllers/public/genres_controller.rb
@@ -1,2 +1,9 @@
 class Public::GenresController < ApplicationController
+
+  def show
+    @genre = Genre.find(params[:id])
+    @posts = Post.where(genre_id: @genre.id)
+    @genres = Genre.all
+  end
+
 end
