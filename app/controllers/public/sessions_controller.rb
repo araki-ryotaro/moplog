0e39c57c5579928b8c9251d7c85e1e43c60a9302
araki-ryotaro <monyo.nyomo@gmail.com>
2022-04-14 11:56:55 +0000

    [add]layout

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -14,7 +14,7 @@ class Public::SessionsController < Devise::SessionsController
   def guest_sign_in
     user = User.guest
     sign_in user
-    redirect_to user_path(user), notice: 'guestuserでログインしました。'
+    redirect_to user_path(user), notice: 'ゲストユーザーでログインしました。'
   end
 
   protected
